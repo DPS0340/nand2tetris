6597b736ef43315ea86259e14c4d646ef87d2621
jiho lee <optional.int@kakao.com>
2020-11-10 18:04:12 +0900

    Add: Project 4, Fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,58 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+  // Put your code here:
+  (MAIN)
+  @SCREEN
+  D=A
+  @SCREEN_PTR
+  M=D-1
+  @24576
+  D=M
+  @ascii
+  M=D
+  @4096
+  D=A
+  @idx
+  M=D+1
+
+  @ascii
+  D=M
+
+  @BLACK
+  D                             ;JNE
+  @WHITE
+  0                             ;JMP
+
+  (FILL)
+  @idx
+  M=M-1
+  D=M
+  @NEXT
+  D                             ;JEQ
+  @color
+  D=M
+  @SCREEN_PTR
+  M=M+1
+  A=M
+  M=D
+  @FILL
+  0                             ;JMP
+
+  (BLACK)
+  @color
+  M=-1
+  @FILL
+  0                             ;JMP
+  (WHITE)
+  M=0
+  @FILL
+  0                             ;JMP
+
+  (NEXT)
+  @SCREEN_ORIGINAL
+  D=M
+  @SCREEN
+  M=D
+  @MAIN
+  0                             ;JMP
