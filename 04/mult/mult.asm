3ef760aaa3340c3abbb6ea4e4d8686be1ac00fb2
jiho lee <optional.int@kakao.com>
2020-11-10 17:24:59 +0900

    Add: Project 4, mult

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -2,8 +2,37 @@
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Mult.asm
-
-// Multiplies R0 and R1 and stores the result in R2.
+// Multiplies R0 and R1 and stores the result in 2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+  // Put your code here.
+
+
+
+  (MULT)
+  @1
+  D=M
+  @i
+  M=D+1
+  @2
+  M=0
+
+  @LOOP
+  0                             ;JMP
+
+  (LOOP)
+  @i
+  M=M-1
+  D=M
+  @END
+  D                             ;JEQ
+  @0
+  D=M
+  @2
+  M=M+D
+  @LOOP
+  0                             ;JMP
+
+  (END)
+  @END
+  0                             ;JMP
